023d8ec540569c3a0a0429b347d07b61ffc525d1
Hasanat902 <abulhasanat902@gmail.com>
2023-01-19 17:08:27 +0600

    added services section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Dental from '../Dental/Dental';
+import InfoCards from '../InfoCards/InfoCards';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div className='mx-5'>
             <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <Dental></Dental>
         </div>
     );
 };

3f0f72f1f747da695fb0de572449c008aca8c606
Hasanat902 <abulhasanat902@gmail.com>
2023-01-21 15:03:05 +0600

    added appointment options in appointmnet page

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,33 @@
 import { format } from 'date-fns';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
+import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({selectedDate}) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+    useEffect( () => {
+        fetch('appointmentOptions.json')
+        .then(res => res.json())
+        .then(data => setAppointmentOptions(data))
+    }, [])
+
     return (
-        <div className='mt-16'>
+        <div className='my-16'>
             <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOption={option}
+                        setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
+                {treatment && <BookingModal treatment={treatment} selectedDate={selectedDate}></BookingModal>}
+            </div>
         </div>
     );
 };

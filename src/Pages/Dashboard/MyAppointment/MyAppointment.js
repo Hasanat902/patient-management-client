4432d5078bb742ae2616cca04d4a92fc3e9f8534
Hasanat902 <abulhasanat902@gmail.com>
2023-01-22 22:50:25 +0600

    implement jwt token

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -11,7 +11,11 @@ const MyAppointment = () => {
     const {data : bookings = []} = useQuery({
         queryKey: ['bookings', user?.email],
         queryFn: async () => {
-            const res = await fetch(url);
+            const res = await fetch(url, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
             const data = await res.json();
             return data;
         }
@@ -33,7 +37,7 @@ const MyAppointment = () => {
           </thead>
           <tbody>
             {
-                bookings.map((booking, i) => <tr>
+                bookings.map((booking, i) => <tr key={booking._id}>
                     <th>{i + 1}</th>
                     <td>{booking.patient}</td>
                     <td>{booking.treatment}</td>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -5,12 +5,19 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
+import useToken from '../../hooks/useToken';
 
 const SignUp = () => {
     const {register, handleSubmit, formState: { errors }} = useForm();
     const {createUser, updateUser} = useContext(AuthContext);
     const [signUpError, setSignUpError] = useState('');
+    const [createdUserEmail, setCreatedUserEmail] = useState('');
+    const [token] = useToken(createdUserEmail);
     const navigate = useNavigate();
+
+    if(token){
+        navigate('/');
+    }
 
     const handleSignUp = data => {
         console.log(data);
@@ -47,10 +54,11 @@ const SignUp = () => {
         })
         .then(res => res.json())
         .then(data => {
-            console.log('save user', data);
-            navigate('/');
+           setCreatedUserEmail(email);
         })
     }
+
+    
 
     return (
         <div className='h-[800px] flex justify-center items-center'>
